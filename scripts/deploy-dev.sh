635597b1b6f4b75c6dcd7c6b7bb43a2a9f9ffc0c
taiatiniyara <taiatiniyara@gmail.com>
2026-01-19 17:52:03 +1200

    Deploying the latest updates

--- scripts/deploy-dev.sh
+++ scripts/deploy-dev.sh
@@ -8,4 +8,8 @@ ssh root@156.67.221.57 << 'EOF'
   cd /root/prism
   git pull origin main
   echo "Server updated with the latest code!"
+
+  npx drizzle-kit push
+  npm run build
+  pm2 reload prism-dev
 EOF
